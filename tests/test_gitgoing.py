b713f56bca7c43f14119134b49be1818f8f1e0b6
Andrew Zaharia <adz213@nyu.edu>
2015-04-11 13:33:39 -0400

    fixed cv

--- tests/test_gitgoing.py
+++ tests/test_gitgoing.py
@@ -38,10 +38,10 @@ def test_std_plus_one(x_norm):
     assert test_std == true_std
 
 def test_cv_broken(x_norm):
-    from gitgoing.gitgoing import std_plus_one, mean_plus_one, cv
+    from gitgoing import std_plus_one, mean_plus_one, cv
 
     test_cv = cv(x_norm)
-    true_cv = std_plus_one(x_norm)/mean_plus_one(x_norm)
+    true_cv = mean_plus_one(x_norm)/std_plus_one(x_norm)
 
     # This test will fail
     assert test_cv == true_cv
